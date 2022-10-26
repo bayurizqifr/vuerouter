c4bcd43db4a649fa642591a3325ecbb47287af45
bayurizqifr <bayurizqi100@gmail.com>
2022-10-26 03:11:46 -0700

    vue router

---
+++ src/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>Contact</h1>
+        <h2>0882 1720 9609</h2>
+        <h2>Bayu Rizqi Fachreza Ramadhan/XII RPL2/11</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contact"
+}
+</script>
